cfe17b0cd672eab314708fd5a6da5e65c9de077f
Magesh <tkmagesh77@gmail.com>
2017-01-04 16:00:09 +0530

    bug tracker - part - 1

--- Day-03/quickstart-master/app/bug-tracker/components/bug-tracker.component.ts
+++ Day-03/quickstart-master/app/bug-tracker/components/bug-tracker.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
+import { IBug } from '../models/IBug';
+
 @Component({
-    template : `<h2>Bug Tracker</h2><hr/>`,
+    template : `
+        <h2>Bug Tracker</h2>
+        <hr/>
+        <section class="stats">
+            <span class="closed">2</span>
+            <span> / </span>
+            <span>{{bugs.length}}</span>
+        </section>
+        <section class="list">
+            <label for="">Bug :</label>
+            <input type="text" #txtBugName>
+            <input type="button" value="Add Bug" (click)="addNewBug(txtBugName.value)">
+            <input type="button" value="Remove Closed" (click)="removeClosed()">
+            <ol>
+                <li [*ngFor]="let bug of bugs">
+                    <span class="bugname" (click)="toggle(bug)" 
+                        [ngClass]="{closed : bug.isClosed}"
+                    >{{bug.name}}</span>
+                </li>
+               
+            </ol>
+        </section>
+    `,
     selector : 'bug-tracker'
 })
 export class BugTrackerComponent{
+    bugs : Array<IBug> = new Array<IBug>()
 
+    addNewBug(bugName:string){
+        this.bugs.push({
+            name : bugName,
+            isClosed : false,
+        })
+    }
+
+    toggle (bug : IBug){
+        bug.isClosed = !bug.isClosed;
+    }
+
+    removeClosed(){
+        this.bugs = this.bugs.filter(function(bug){ 
+            return  !bug.isClosed
+        });
+    }
 }
